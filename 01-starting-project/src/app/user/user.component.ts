0a5fb82f160be584f00509f8753b80554fc84976
PrakkiKhyathi <khyathiprakki@gmail.com>
2026-02-06 12:35:58 +0530

    Angular-first-project-Easy Task

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,40 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  Output,
+  signal,
+} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+import { type User } from './user.model';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// type User={
+//     id: string;
+//     avatar: string;
+//     name: string;
+// }
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output()
+  select = new EventEmitter<string>();
+  get imagePath() {
+    return '/assets/users/' + this.user.avatar;
+  }
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
